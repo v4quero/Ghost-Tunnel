7d172cd1bddbb23c0971c0d1be6e4b87365a6ce8
Brandon R <65254705+v4quero@users.noreply.github.com>
2020-09-01 08:33:22 -0600

    Update ghost.sh

--- ghost.sh
+++ ghost.sh
@@ -1,5 +1,25 @@
 #!/bin/bash
 setterm -term linux -back black -fore green --clear all
+
+##V4quero logo
+echo -e "................................................................................"
+echo -e ".....................................::::::....................................."
+echo -e ".............................:*VVVVVVVVVVVVVVVVVV*:............................."
+echo -e "...........................*VVVVVVVVVVVVVVVVVVVVVVVV*..........................."
+echo -e "..........................*VVVVVVVVVVVVVVVVVVVVVVVVVV:.........................."
+echo -e "..........................VVVVVVV*VVVVVVVVVVV*V*VVVVV*.........................."
+echo -e "..........................VVVVV**VVVVVVVVVVVVVV*VVVVVV.........................."
+echo -e ".........................:VVV***VVVVVVVVVVVVVVVV***VVV:........................."
+echo -e ".........................*****VVVVVVVVVVVVVVVVVVVVV**V*........................."
+echo -e ".........................***VVVVVVVVVVVVVVVVVVVVVVVV***...................:....."
+echo -e ".....***.................*VVVVVVVVVVVVVVVVVVVVVVVVVVVV*.................*V*....."
+echo -e "......**V*:.............:VVVVVVVVVVVVVVVVVVVVVVVVVVVVVV:.............:VVV*......"
+echo -e ".......*VVVVV**:........*VFFFVVVVVVVVVVVVVVVVVVVVVVVVVV*........:**VVVVV*......."
+echo -e "........:*VVVVVVVVV*******VVV****V*VV**V***VVVVV*************VVVVVVVVVV:........"
+echo -e "..........:**VVVVVVVVV*VVVFFFIIIIIMMMMMMMMMMMMMIIIIIFFVVVVVVVVVVVVVV*:.........."
+echo -e "...............::******VVVFFIIIIIIIIIIIIIIIIIIIIIIIIFFVVVVV****::..............."
+echo -e "...........................:::*****VVVVVVVVVVVVVVVVVV***:......................."
+echo -e ".................................----V4QUERO----................................"
 echo "You have initiated Ghost_tunnel, do you wish to continue?"
 #continue command
 read -n1 -p "Press [Enter] to continue."
